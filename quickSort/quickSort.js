1b8f236465ce216b25bd1679df4d6c45c77b975a
jerryHZLiang <798895110@qq.com>
2019-02-27 14:57:22 +0800

    Update quickSort.js

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -18,6 +18,34 @@ var arr = [10, 34, 21, 47, 3, 28]
 var result = quickSort(arr)
 console.log(result)
 
+
+//方法二
+// function quickSort(arr){
+// 	function _quickSort(arr,start,end){
+//   	if(start >=end) return;
+//     let key = arr[end];
+//     let left = start
+//     let right = end-1;
+//     while(left<right){
+//     	if(arr[left]<key && left<right) left++;
+//       if(arr[right]>key && right>right) right--;
+//       [arr[left],arr[right]]=[arr[right],arr[left]]
+//     }
+//     if(arr[left]>=rr[end]){
+//     	[arr[left],arr[end]]=[arr[end],arr[left]]
+//     }else{
+//        left++
+//     }
+//     _quickSort(arr, start, left - 1)
+//     _quickSort(arr, left + 1, end)
+//   }
+//   _quickSort(arr, 0, arr.length - 1)
+//   return arr
+// }
+
+// var arr = [10, 34, 21, 47, 3, 28]
+// var s = quickSort(arr)
+// console.log(s)
 // let arr = randomArr(10000, 100)
 // console.time('quickSort')
 // quickSort(arr)
